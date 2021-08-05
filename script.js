d514a2f2d51cb689a0b527f72ac7a5e69b417f40
Flowwws <82537408+Flowwws@users.noreply.github.com>
2021-08-05 15:59:36 +0200

    ajout img, commentaire ,déplacement js
    Déplacement code js dans le fichier script.js

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ btnClose.addEventListener('click',closeModal);
 function closeModal(){
 	overlay.style.display='none';
 }
+
+
+/* Eyes Tracking*/
+
+var balls = document.getElementsByClassName("ball-noah");
+        var balls1 = document.getElementsByClassName("ball-florian");
+        var balls2 = document.getElementsByClassName("ball-chantal");
+        var balls3 = document.getElementsByClassName("ball-adam");
+        document.onmousemove = function(){
+          var x = event.clientX * 100 / window.innerWidth + "%";
+          var y = event.clientY * 100 / window.innerHeight + "%";
+
+
+          for(var i=0;i<2;i++){
+            balls[i].style.left = x;
+            balls[i].style.top = y;
+            balls[i].style.transform = "translate(-"+x+",-"+y+")";
+            balls1[i].style.left = x;
+            balls1[i].style.top = y;
+            balls1[i].style.transform = "translate(-"+x+",-"+y+")";
+            balls2[i].style.left = x;
+            balls2[i].style.top = y;
+            balls2[i].style.transform = "translate(-"+x+",-"+y+")";
+            balls3[i].style.left = x;
+            balls3[i].style.top = y;
+            balls3[i].style.transform = "translate(-"+x+",-"+y+")";
+          }
+        }
