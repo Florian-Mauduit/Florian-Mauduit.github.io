bf8c4f2dd756eafbe1a18f75e22be589ec80a1b6
Flowwws <82537408+Flowwws@users.noreply.github.com>
2021-08-10 10:15:39 +0200

    ajout de la popup pour le deuxième boutons
    suppression de la partie css qui sert a rien

--- js/script.js
+++ js/script.js
@@ -14,6 +14,20 @@ function closeModal(){
 }
 
 
+var bleu = document.getElementById('bleu');
+var overlaybleu = document.getElementById('overlaybleu');
+bleu.addEventListener('click',openMoadl);
+function openMoadl() {
+overlaybleu.style.display='block';
+}
+
+var btnClosebleu = document.getElementById('btnClosebleu');
+btnClosebleu.addEventListener('click',closeModal);
+function closeModal(){
+  overlaybleu.style.display='none';
+}
+
+
 /* Eyes Tracking*/
 
 var balls = document.getElementsByClassName("ball-noah");
